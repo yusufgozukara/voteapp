94ea0f9a9a4f67d024498df670c94f114abc212d
Yusuf Gözükara <101390306+yusufgozukara@users.noreply.github.com>
2023-05-31 14:58:31 +0300

    invalidvote function added

--- src/components/InvalidVote/InvalidVote.js
+++ src/components/InvalidVote/InvalidVote.js
@@ -1,14 +1,28 @@
 import React from "react";
+import { useState } from "react";
 
 const InvalidVote = () => {
+  const [invalidVote, setInvalidVote] = useState(0);
+
+  const invalidPlus = () => {
+    console.log("geçersiz arttı.");
+  };
+  const invalidMinus = () => {
+    console.log("geçersiz azaldı.");
+  };
+
   return (
     <div className="invalid">
       <div className="invalidfirstdiv">
         <p>Geçersiz Oy</p>
       </div>
       <div className="invalidseconddiv">
-        <button className="invalidMinus">-</button>
-        <button className="invalidPlus">+</button>
+        <button onClick={() => invalidMinus()} className="invalidMinus">
+          -
+        </button>
+        <button onClick={() => invalidPlus()} className="invalidPlus">
+          +
+        </button>
       </div>
     </div>
   );
